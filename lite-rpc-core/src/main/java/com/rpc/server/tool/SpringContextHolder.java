962aee91d7e771bd95edeb36bb4727537f379cde
ShiYue1026 <shiyue20011026@126.com>
2025-03-03 22:07:06 +0800

    使用Prometheus + Grafana进行监控

---
+++ lite-rpc-core/src/main/java/com/rpc/server/tool/SpringContextHolder.java
@@ -0,0 +1,24 @@
+package com.rpc.server.tool;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContextHolder implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        applicationContext = context;
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        if (applicationContext == null) {
+            throw new IllegalStateException("ApplicationContext 未初始化");
+        }
+        return applicationContext.getBean(clazz);
+    }
+}
